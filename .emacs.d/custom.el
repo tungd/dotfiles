eb2a18eac6b3218b3db251503ca55416d644a54f
Tung Dao <me@tungdao.com>
2016-10-10 20:40:10 +0700

    [emacs] a bunch of unrelated stuffs

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(package-selected-packages
+   (quote
+    (ztree yasnippet yaml-mode window-numbering wgrep-ag web-mode use-package undo-tree sublime-themes smex smartparens smart-mode-line realgud rainbow-mode pyvenv purescript-mode psc-ide projectile paren-completer ox-pandoc ob-http nlinum nix-mode multiple-cursors multi-term magit kite-mini js2-mode intero indent-guide hl-todo flx fish-mode expand-region exec-path-from-shell emmet-mode editorconfig dumb-jump dockerfile-mode diff-hl crux counsel company-statistics comment-dwim-2 better-defaults base16-theme anzu alchemist ag)))
  '(safe-local-variable-values
    (quote
     ((eval add-hook
