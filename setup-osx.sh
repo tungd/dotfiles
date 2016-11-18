8176f01d05361e1ab55a807b691af63b1e067b55
Tung Dao <me@tungdao.com>
2016-11-18 08:07:08 +0700

    disable shadow in screenshots

--- setup-osx.sh
+++ setup-osx.sh
@@ -3,3 +3,6 @@ defaults write NSGlobalDomain KeyRepeat -int 0
 defaults write -g NSUserKeyEquivalents -dict-add 'Emoji & Symbols' '@^s'
 
 echo 'pinentry-program /Users/tung/.nix-profile/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac' >> ~/.gnupg/gpg-agent.conf
+
+defaults write com.apple.screencapture disable-shadow -bool true
+killall SystemUIServer
