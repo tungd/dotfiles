bd79b5e8c4ff4e0b29f2b0be73d1bacdc2dac416
Tung Dao <me@tungdao.com>
2018-03-13 22:03:32 +0700

    I get my keyboard fixed, yay! :)

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,37 +1,5 @@
 local logger = hs.logger.new('init', 'info')
 
-local eventTypes = hs.eventtap.event.types
-local eventProps = hs.eventtap.event.properties
-
--- Fix deadkey 'n' on my internal keyboard
-hs.eventtap.new({eventTypes.keyDown}, function(event)
-      local keyCode = event:getProperty(eventProps.keyboardEventKeycode)
-      local keyboardType = event:getProperty(eventProps.keyboardEventKeyboardType)
-      local modifiers = hs.eventtap.checkKeyboardModifiers()
-
-      logger.d(
-         "keyDown [keyCode: " .. keyCode ..
-            ", keyboardType: " .. keyboardType ..
-            -- ", modifiers: " .. modifiers["alt"] ..
-            "]")
-
-      -- 43 is the internal keyboard
-      if keyboardType == 43 and keyCode == hs.keycodes.map["\\"] then
-         local e = event:copy()
-         if modifiers["alt"] then
-            -- Unless the 'option' key is down, clear the flag so it is still
-            -- '\\', but passing through other modifiers such as
-            -- 'shift'. Doesn't work well with repeat but it's fine for me
-            modifiers["alt"] = nil
-            e:setFlags(modifiers)
-            return true, {e}
-         end
-
-         e:setKeyCode(hs.keycodes.map["n"])
-         return true, {e}
-      end
-end):start()
-
 function focus_prev()
    local windows = hs.window.filter.default:getWindows(
       hs.window.filter.sortByLastFocused
