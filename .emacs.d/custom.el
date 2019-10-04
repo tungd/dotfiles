d7bc85c141af35beffdc28c3e1aa8f0414505dfc
Tung Dao <me@tungdao.com>
2019-10-04 10:03:52 +0700

    change: use ivy-posframe

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,11 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("071f5702a5445970105be9456a48423a87b8b9cfa4b1f76d15699b29123fb7d8" default))
  '(datetime-timezone 'Asia/Ho_Chi_Minh)
  '(package-selected-packages
-   '(expand-region ivy-posframe use-package anzu tuareg yasnippet yaml-mode window-numbering web-mode undo-tree tango-plus-theme swift-mode solidity-mode smex smartparens smart-mode-line rust-mode request rainbow-mode rainbow-delimiters pyvenv py-isort ob-sql-mode ob-http nginx-mode markdown-mode magit kotlin-mode ivy-historian indent-guide highlight-numbers groovy-mode go-mode exec-path-from-shell emmet-mode eglot dumb-jump doom-themes dockerfile-mode diminish diff-hl crux counsel company-statistics comment-dwim-2 ag add-node-modules-path 0blayout))
+   '(docker haskell-mode dune utop eldoc-box eldoc-overlay expand-region ivy-posframe use-package anzu tuareg yasnippet yaml-mode window-numbering web-mode undo-tree tango-plus-theme swift-mode solidity-mode smex smartparens smart-mode-line rust-mode request rainbow-mode rainbow-delimiters pyvenv py-isort ob-sql-mode ob-http nginx-mode markdown-mode magit kotlin-mode ivy-historian indent-guide highlight-numbers groovy-mode go-mode exec-path-from-shell emmet-mode eglot dumb-jump doom-themes dockerfile-mode diminish diff-hl crux counsel company-statistics comment-dwim-2 ag add-node-modules-path 0blayout))
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook
+           (lambda nil
+             (org-babel-tangle)
+             (byte-compile-file "~/.emacs.d/init.el"))
+           nil t)
+     (eval add-hook 'after-save-hook
            (lambda nil
              (org-babel-tangle)
              (async-byte-compile-file "~/.emacs.d/init.el"))
