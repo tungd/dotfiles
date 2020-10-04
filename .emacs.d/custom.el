a469eb706383745c906e56f0ab1898f0df40bd8c
Tung Dao <me@tungdao.com>
2020-10-05 00:22:28 +0700

    cleanup

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,14 +6,22 @@
  '(ansi-color-names-vector
    ["#1B2B34" "#EC5f67" "#99C794" "#FAC863" "#6699CC" "#E27E8D" "#5FB3B3" "#D8DEE9"])
  '(custom-safe-themes
-   '("2cdc13ef8c76a22daa0f46370011f54e79bae00d5736340a5ddfe656a767fddf" "93ed23c504b202cf96ee591138b0012c295338f38046a1f3c14522d4a64d7308" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" "7d708f0168f54b90fc91692811263c995bebb9f68b8b7525d0e2200da9bc903c" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "e47e52c3dac4c3b6a77e32dcdee6de63858277247485f7c569b35c04de9a1501" "54cf3f8314ce89c4d7e20ae52f7ff0739efb458f4326a2ca075bf34bc0b4f499" default))
+   '("8b8fd1c936a20b5ca6afe22e081798ffb5e7498021515accadc20aab3517d402" "9b7f37885eec6ef0441bae9c5ea4a1dd2484eef4342aa3f88d1691722f769fba" "45feb1f130c54e0fc116faa71c784562b41009ffc908cf5cef06b6df4bb60a9a" "d3856ef5a26c9f375f4a084af2e89fa215212fe44540deea941d264d00efead4" "1d78d6d05d98ad5b95205670fe6022d15dabf8d131fe087752cc55df03d88595" "e7ba99d0f4c93b9c5ca0a3f795c155fa29361927cadb99cfce301caf96055dfd" "16ab866312f1bd47d1304b303145f339eac46bbc8d655c9bfa423b957aa23cc9" "edf1f9e74600cac84368d8c1ae2158db85217c3a02e3b1470545462a64cea016" "bc7627a5d14001acb3237151df7ccb413b57e4a820295ec24562c132efcacb2e" "fc6697788f00629cd01f4d2cc23f1994d08edb3535e4c0facef6b7247b41f5c7" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "2cdc13ef8c76a22daa0f46370011f54e79bae00d5736340a5ddfe656a767fddf" "93ed23c504b202cf96ee591138b0012c295338f38046a1f3c14522d4a64d7308" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" "7d708f0168f54b90fc91692811263c995bebb9f68b8b7525d0e2200da9bc903c" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "e47e52c3dac4c3b6a77e32dcdee6de63858277247485f7c569b35c04de9a1501" "54cf3f8314ce89c4d7e20ae52f7ff0739efb458f4326a2ca075bf34bc0b4f499" default))
  '(fci-rule-color "#C0C5CE")
+ '(global-hl-line-mode nil)
  '(jdee-db-active-breakpoint-face-colors (cons "#1B2B34" "#FAC863"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#1B2B34" "#99C794"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#1B2B34" "#A7ADBA"))
+ '(linum-format " %5i ")
+ '(notmuch-search-line-faces
+   '(("unread" :foreground "#aeee00")
+     ("flagged" :foreground "#0a9dff")
+     ("deleted" :foreground "#ff2c4b" :bold t)))
+ '(nrepl-message-colors
+   '("#183691" "#969896" "#a71d5d" "#969896" "#0086b3" "#795da3" "#a71d5d" "#969896"))
  '(objed-cursor-color "#EC5f67")
  '(package-selected-packages
-   '(multi-vterm vterm-toggle doom-themes golden-ratio ein clojure-inf clojure kotlin-mode feature-mode company-box company-prescient typescript-mode mini-modeline vterm popup-complete which-key completions-frame poetry fancy-dabbrev smartparens selectrum-prescient prescient selectrum deadgrep eshell-git-prompt tango-plus-theme highlight-numbers ob-http yaml-mode nginx-mode company-terraform terraform-doc terraform-mode dockerfile-mode docker rust-mode lsp-haskell haskell-mode py-isort rainbow-mode emmet-mode web-mode magit with-editor eshell-z eshell-up eshell-toggle yasnippet undo-tree expand-region ctrlf visual-regexp crux comment-dwim-2 window-numbering exec-path-from-shell use-package delight))
+   '(tommyh-theme greymatters-theme mustang-theme github-theme twilight-theme badwolf-theme cloud-theme paper-theme tangotango-theme sublime-themes multi-vterm vterm-toggle doom-themes golden-ratio ein clojure-inf clojure kotlin-mode feature-mode company-prescient typescript-mode mini-modeline vterm popup-complete which-key completions-frame poetry fancy-dabbrev smartparens selectrum-prescient prescient selectrum deadgrep eshell-git-prompt tango-plus-theme highlight-numbers ob-http yaml-mode nginx-mode company-terraform terraform-doc terraform-mode dockerfile-mode docker rust-mode lsp-haskell haskell-mode py-isort rainbow-mode emmet-mode web-mode magit with-editor eshell-z eshell-up eshell-toggle yasnippet undo-tree expand-region ctrlf visual-regexp crux comment-dwim-2 window-numbering exec-path-from-shell use-package delight))
  '(pdf-view-midnight-colors (cons "#D8DEE9" "#1B2B34"))
  '(recentf-mode t)
  '(rustic-ansi-faces
@@ -51,6 +59,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
  '(diff-hl-change ((t (:inherit nil :background nil :foreground "#deae3e"))))
  '(diff-hl-delete ((t (:inherit nil :background nil :foreground "#ff0000"))))
  '(diff-hl-insert ((t (:inherit nil :background nil :foreground "#81af34"))))
