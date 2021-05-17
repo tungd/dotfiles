b0f78351810796fcefc731da1fb3bb72c49643f7
Tung Dao <me@tungdao.com>
2021-05-18 02:13:06 +0700

    more cleanup:
    - remove lesser used modes such as Solidity and Clojure mode
    - add mini-frame
    - switch from flymake to flycheck to reduce editor lock-up

--- emacs/custom.el
+++ emacs/custom.el
@@ -5,8 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("1d78d6d05d98ad5b95205670fe6022d15dabf8d131fe087752cc55df03d88595" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+ '(flycheck-keymap-prefix (kbd "C-c e"))
+ '(mini-frame-show-paramters '((top . 0) (width . 90) (left . 0)))
  '(package-selected-packages
-   '(github-theme smart-mode-line yasnippet yaml-mode which-key web-mode visual-regexp use-package typescript-mode tuareg terraform-mode terraform-doc tango-plus-theme sqlformat smartparens selectrum-prescient reason-mode rainbow-mode py-isort ob-http ob-graphql nginx-mode magit lsp-pyright inf-clojure highlight-numbers haskell-mode golden-ratio fsharp-mode feature-mode expand-region exec-path-from-shell evil-surround eshell-z eshell-up eshell-toggle emmet-mode dune dockerfile-mode docker delight ctrlf crux consult company-prescient comment-dwim-2 ag ace-window))
+   '(mini-frame flycheck github-theme smart-mode-line yasnippet yaml-mode which-key web-mode visual-regexp use-package typescript-mode tuareg terraform-mode terraform-doc tango-plus-theme sqlformat smartparens selectrum-prescient reason-mode rainbow-mode py-isort ob-http ob-graphql nginx-mode magit lsp-pyright highlight-numbers haskell-mode golden-ratio feature-mode expand-region exec-path-from-shell evil-surround eshell-z eshell-up eshell-toggle emmet-mode dune dockerfile-mode docker delight ctrlf crux consult company-prescient comment-dwim-2 ag ace-window))
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook
            (lambda nil
