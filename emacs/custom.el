88945c08efe4aa4e56772ec4054a9e6ff2d596a1
Tung Dao <me@tungdao.com>
2021-05-31 23:26:04 +0700

    add: function to change the font size based on the monitor's PPI

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,18 +3,53 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282b33" "#e1c1ee" "#5b94ab" "#cfcf9c" "#819cd6" "#a6c1e0" "#7289bc" "#c6c6c6"])
  '(custom-safe-themes
-   '("1d78d6d05d98ad5b95205670fe6022d15dabf8d131fe087752cc55df03d88595" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+   '("1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "1d78d6d05d98ad5b95205670fe6022d15dabf8d131fe087752cc55df03d88595" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+ '(fci-rule-color "#888395")
  '(flycheck-keymap-prefix (kbd "C-c e"))
+ '(jdee-db-active-breakpoint-face-colors (cons "#222228" "#819cd6"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#222228" "#5b94ab"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#222228" "#515462"))
  '(mini-frame-show-paramters '((top . 0) (width . 90) (left . 0)))
+ '(objed-cursor-color "#e1c1ee")
  '(package-selected-packages
-   '(mini-frame flycheck github-theme smart-mode-line yasnippet yaml-mode which-key web-mode visual-regexp use-package typescript-mode tuareg terraform-mode terraform-doc tango-plus-theme sqlformat smartparens selectrum-prescient reason-mode rainbow-mode py-isort ob-http ob-graphql nginx-mode magit lsp-pyright highlight-numbers haskell-mode golden-ratio feature-mode expand-region exec-path-from-shell evil-surround eshell-z eshell-up eshell-toggle emmet-mode dune dockerfile-mode docker delight ctrlf crux consult company-prescient comment-dwim-2 ag ace-window))
+   '(swift-mode consult-lsp kaolin-themes doom-themes lsp-python-ms consult-flycheck mini-frame flycheck github-theme smart-mode-line yasnippet yaml-mode which-key web-mode visual-regexp use-package typescript-mode tuareg terraform-mode terraform-doc tango-plus-theme sqlformat selectrum-prescient reason-mode rainbow-mode py-isort ob-http ob-graphql nginx-mode magit highlight-numbers haskell-mode golden-ratio feature-mode expand-region exec-path-from-shell evil-surround eshell-z eshell-up eshell-toggle emmet-mode dune dockerfile-mode docker delight ctrlf crux consult company-prescient comment-dwim-2 ag ace-window))
+ '(pdf-view-midnight-colors (cons "#c6c6c6" "#282b33"))
+ '(rustic-ansi-faces
+   ["#282b33" "#e1c1ee" "#5b94ab" "#cfcf9c" "#819cd6" "#a6c1e0" "#7289bc" "#c6c6c6"])
  '(safe-local-variable-values
-   '((eval add-hook 'after-save-hook
+   '((python-shell-interpreter-args . "run outsight/manage.py shell")
+     (python-shell-interpreter . "poetry")
+     (python-shell-interpreter . "/opt/local/bin/python3")
+     (eval add-hook 'after-save-hook
            (lambda nil
              (org-babel-tangle)
              (byte-recompile-file "~/.config/emacs/init.el"))
-           nil t))))
+           nil t)))
+ '(vc-annotate-background "#282b33")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#5b94ab")
+    (cons 40 "#81a7a6")
+    (cons 60 "#a8bba1")
+    (cons 80 "#cfcf9c")
+    (cons 100 "#c1cab2")
+    (cons 120 "#b3c5c9")
+    (cons 140 "#a6c1e0")
+    (cons 160 "#a6c1e0")
+    (cons 180 "#a6c1e0")
+    (cons 200 "#a6c1e0")
+    (cons 220 "#b9c1e4")
+    (cons 240 "#cdc1e9")
+    (cons 260 "#e1c1ee")
+    (cons 280 "#bda5cb")
+    (cons 300 "#998aa8")
+    (cons 320 "#756f85")
+    (cons 340 "#888395")
+    (cons 360 "#888395")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
